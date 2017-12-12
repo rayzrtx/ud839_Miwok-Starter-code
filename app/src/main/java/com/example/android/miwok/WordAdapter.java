ea2bd0e8fdb51c222363a543c2a7c6b72f6e539c
Ray Linares <rayl77@gmail.com>
2017-12-12 13:36:23 -0600

    Updated comments in WordAdapter and list_item

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -15,25 +15,49 @@ import java.util.ArrayList;
  * Created by raylinares on 12/11/17.
  */
 
+/**
+ * {@link WordAdapter} is an {@link ArrayAdapter} that can provide the layout for each list item
+ * based on a data source, which is a list of {@link Word} objects.
+ */
 public class WordAdapter extends ArrayAdapter<Word> {
 
+    /**
+     * Create a new {@link WordAdapter} object.
+     *
+     * @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param words is the list of {@link Word}s to be displayed.
+     */
     public WordAdapter(Activity context, ArrayList<Word> words){
         super(context, 0, words);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if an existing view is being reused, otherwise inflate the view
         View view = convertView;
         if (view == null){
             view = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
 
+        // Get the {@link Word} object located at this position in the list
         Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text_view.
         TextView miwokTextView = (TextView) view.findViewById(R.id.miwok_text_view);
+
+        // Get the Miwok translation from the currentWord object and set this text on
+        // the Miwok TextView.
         miwokTextView.setText(currentWord.getMiwokTranslation());
 
+        // Find the TextView in the list_item.xml layout with the ID default_text_view.
         TextView defaultTextView = (TextView) view.findViewById(R.id.english_text_view);
+
+        // Get the default translation from the currentWord object and set this text on
+        // the default TextView.
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView in activity_numbers.
         return view;
     }
 }
