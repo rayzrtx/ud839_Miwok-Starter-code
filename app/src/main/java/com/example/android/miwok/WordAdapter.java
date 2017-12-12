4e4ad817875525d42390f70d27658e2178acf7ed
Ray Linares <rayl77@gmail.com>
2017-12-11 23:29:41 -0600

    Created new Word class. Created new WordAdapter class. Added Miwok tranlations to ArrayList.

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by raylinares on 12/11/17.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word> words){
+        super(context, 0, words);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null){
+            view = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+        TextView miwokTextView = (TextView) view.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) view.findViewById(R.id.english_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+        return view;
+    }
+}
